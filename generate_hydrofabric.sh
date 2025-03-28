0e1fc6a8d2038a2314522365750086a3af521a55
Josh Cunningham <josh.cu@gmail.com>
2025-03-28 13:43:46 -0500

    comment out destructive s3 commands

--- generate_hydrofabric.sh
+++ generate_hydrofabric.sh
@@ -5,5 +5,5 @@ docker cp hf_container:/output/conus_nextgen.gpkg .
 docker cp hf_container:/output/conus_nextgen.tar.gz .
 docker kill hf_container
 docker rm hf_container
-aws s3 cp ./conus_nextgen.gpkg s3://communityhydrofabric/hydrofabrics/community/
-aws s3 cp ./conus_nextgen.tar.gz s3://communityhydrofabric/hydrofabrics/community/
+#aws s3 cp ./conus_nextgen.gpkg s3://communityhydrofabric/hydrofabrics/community/
+#aws s3 cp ./conus_nextgen.tar.gz s3://communityhydrofabric/hydrofabrics/community/
--- generate_vpus.sh
+++ generate_vpus.sh
@@ -4,5 +4,5 @@ docker run -d --name vpu_container hf sleep infinity
 docker cp vpu_container:/workspace VPU
 docker kill vpu_container
 docker rm vpu_container
-aws configure set s3.max_concurrent_requests 200
-aws s3 sync VPU s3://communityhydrofabric/hydrofabrics/community/VPU
+#aws configure set s3.max_concurrent_requests 200
+#aws s3 sync VPU s3://communityhydrofabric/hydrofabrics/community/VPU
